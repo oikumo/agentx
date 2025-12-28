5ace90ac187a696ee9431a6b88a417d79459d964
oikumo <oikumo@gmail.com>
2025-12-28 12:51:13 -0300

    [WIP] RAG

--- agent_x/app/ai/rag/rag_pdf/rag_pdf.py
+++ agent_x/app/ai/rag/rag_pdf/rag_pdf.py
@@ -18,33 +18,26 @@ from agent_x.modules.vector_store.faiss.vector_store_faiss import create_faiss
 
 load_dotenv()
 
-if __name__ == "__main__":
-    print("hi")
-    pdf_path = "/home/oikumo/Proyectos/llm-projects/agent-x/resources/react.pdf"
-    vectorstore_path = "/home/oikumo/Proyectos/llm-projects/agent-x/local/faiss_index_react"
-    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
-
-    #llm = get_llama_cpp_llm()
-    llm = get_local_llm_qwen3()
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
-
-    #llm = OpenAI()
-    #embeddings = OpenAIEmbeddings()
-
+def rag_pdf(pdf_path, vectorstore_path, retrieval_qa_chat_prompt, llm, embeddings):
     docs = pdf_loader(pdf_path)
-
-    new_vectorstore = create_faiss(
-        vectorstore_path,
-        docs,
-        embeddings
-    )
-
-    combine_docs_chain = create_stuff_documents_chain(
-        llm, retrieval_qa_chat_prompt
-    )
-    retrieval_chain = create_retrieval_chain(
-        new_vectorstore.as_retriever(), combine_docs_chain
-    )
+    new_vectorstore = create_faiss(vectorstore_path, docs, embeddings)
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    retrieval_chain = create_retrieval_chain(new_vectorstore.as_retriever(), combine_docs_chain)
 
     res = retrieval_chain.invoke({"input": "Give me the gist of ReAct in 3 sentences, the output MUST BE in bullet points"})
-    print(res["answer"])
+    return res["answer"]
+
+if __name__ == "__main__":
+    print("rag_pdf")
+    answer = rag_pdf(
+        pdf_path="/home/oikumo/Proyectos/llm-projects/agent-x/resources/react.pdf",
+        vectorstore_path = "/home/oikumo/Proyectos/llm-projects/agent-x/local/faiss_index_react",
+        retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat"),
+        # llm = get_llama_cpp_llm()
+        llm = get_local_llm_qwen3(),
+        embeddings = OllamaEmbeddings(model="nomic-embed-text")
+        # llm = OpenAI()
+        # embeddings = OpenAIEmbeddings()
+    )
+
+    print(answer)
