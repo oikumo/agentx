fb2ff272ea04230375e45b21a5982188d5337830
oikumo <oikumo@gmail.com>
2026-01-18 13:12:42 -0300

    [WIP] Refactor extract modules

--- agent_x/app/controlllers/main_controller/commands/llm_graph_commands.py
+++ agent_x/app/controlllers/main_controller/commands/llm_graph_commands.py
@@ -1,4 +1,4 @@
-from agent_x.app.ai.graph_reflector_chain.graph_chains import graph_chains
+from agent_x.modules.ai.langgraph.graph_reflector_chain import graph_chains
 from agent_x.modules.ai.langgraph.graph_reflexion_agent.graph_reflexion_agent import graph_reflexion_agent
 from agent_x.core.controllers.command_line_controller.command import Command
 from agent_x.modules.ai.langgraph.graph_simple.graph_simple import graph_simple
