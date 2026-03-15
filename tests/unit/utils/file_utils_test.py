0abd94625afea593b51a71d07637c36468a99ea0
oikumo <oikumo@gmail.com>
2026-03-15 18:34:39 -0300

    [WIP] Big Refactor

--- tests/unit/utils/file_utils_test.py
+++ tests/unit/utils/file_utils_test.py
@@ -16,10 +16,9 @@ class FileUtilsTest(unittest.TestCase):
 
     def test_directory_exits(self):
         directory_name = "test_name_exists"
-        base_directory = "utils_test"
         path = create_directory_with_timestamp(directory_name, TEST_SANDBOX_FOLDER)
         self.assertTrue(directory_name in path)
-        self.assertTrue(base_directory in path)
+        self.assertTrue(TEST_SANDBOX_FOLDER in path)
         self.assertTrue(directory_exists(path))
 
         permanently_delete_test_framework_directories(self, path)
