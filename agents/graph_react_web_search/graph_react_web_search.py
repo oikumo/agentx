f39b784cbdea9814e07bd76c1a0aa4540e78f760
oikumo <oikumo@gmail.com>
2026-03-29 18:21:37 -0300

    [WIP] Refactor  * [WIP] Centralize agent in a module

--- agents/graph_react_web_search/graph_react_web_search.py
+++ agents/graph_react_web_search/graph_react_web_search.py
@@ -26,28 +26,57 @@ def triple(num: float) -> float:
     param num: a number to triple
     returns: the triple of the input number
     """
+    print(f"TOOL: triple, num: {num}")
     return float(num) * 3
 
 class GraphReactWebSearch:
     def __init__(self, llm: BaseChatModel, max_search_results: int):
-        self.llm = llm
         self.tools = [
             TavilySearch(max_results=max_search_results),
             triple
         ]
+        self.llm = llm.bind_tools(self.tools)
 
-        self.llm = self.llm.bind_tools(self.tools)
+    def run(self):
+        print("REACT LangGraph with Function Calling")
+        tool_node = ToolNode(self.tools)
+
+        flow = StateGraph(MessagesState)
+
+        flow.add_node(AGENT_REASON, self.run_agent_reasoning)
+        flow.set_entry_point(AGENT_REASON)
+        flow.add_node(ACT, tool_node)
+        flow.add_conditional_edges(AGENT_REASON, self.should_continue, {END: END, ACT: ACT})
+        flow.add_edge(ACT, AGENT_REASON)
+
+        app = flow.compile()
+        # app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+
+        print("GRAPH execute")
+        res = app.invoke(
+            {
+                "messages": [
+                    HumanMessage(
+                        content="What is the temperature in Santiago and Tokio? List it and then triple the Tokio one"
+                    )
+                ]
+            }
+        )
+        print(res["messages"][LAST].content)
 
     def run_agent_reasoning(self, state: MessagesState) -> MessagesState:
         """
         Run the agent reasoning node.
         """
+        print("NODE: Reasoning")
         response = self.llm.invoke(
             [{"role": "system", "content": SYSTEM_MESSAGE}, *state["messages"]]
         )
         return {"messages": [response]}
 
     def should_continue(self, state: MessagesState) -> str:
+        print("NODE: Should continue")
+
         last_message = state["messages"][LAST]
         # Check if the last message has tool calls
         if hasattr(last_message, "tool_calls") and last_message.tool_calls:
@@ -59,36 +88,3 @@ class GraphReactWebSearch:
         ):
             return ACT
         return END
-
-
-    def run(self):
-        print(f"graph simple")
-        print("Hello ReAct LangGraph with Function Calling")
-
-        flow = StateGraph(MessagesState)
-        tool_node = ToolNode(self.tools)
-
-        flow.add_node(AGENT_REASON, self.run_agent_reasoning)
-        flow.set_entry_point(AGENT_REASON)
-        flow.add_node(ACT, tool_node)
-
-        flow.add_conditional_edges(AGENT_REASON, self.should_continue, {END: END, ACT: ACT})
-
-        flow.add_edge(ACT, AGENT_REASON)
-
-        local_path = Path.cwd() / "local"
-        print(f"WWWWWWWWWWWWWWWW: {local_path}")
-
-        app = flow.compile()
-        app.get_graph().draw_mermaid_png(output_file_path="flow.png")
-
-        res = app.invoke(
-            {
-                "messages": [
-                    HumanMessage(
-                        content="What is the temperature in Santiago and Tokio? List it and then triple the Tokio one"
-                    )
-                ]
-            }
-        )
-        print(res["messages"][LAST].content)
